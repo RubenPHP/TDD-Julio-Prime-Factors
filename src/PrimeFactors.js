38e24830ed938b89eec771cc79d6966fccad0dd4
Ruben <ruben@ruben-Aspire-6530G.(none)>
2015-02-27 15:09:45 +0100

    Init project

---
+++ src/PrimeFactors.js
@@ -0,0 +1,20 @@
+function PrimeFactors() {
+}
+
+
+/*
+Player.prototype.pause = function() {
+  this.isPlaying = false;
+};
+
+Player.prototype.resume = function() {
+  if (this.isPlaying) {
+    throw new Error("song is already playing");
+  }
+
+  this.isPlaying = true;
+};
+
+Player.prototype.makeFavorite = function() {
+  this.currentlyPlayingSong.persistFavoriteStatus(true);
+};*/
