889de444633c717e56086d0af54fe6806161f6c0
Ruben <ruben@ruben-Aspire-6530G.(none)>
2015-03-03 16:06:10 +0100

    Fixing

--- src/PrimeFactors.js
+++ src/PrimeFactors.js
@@ -16,20 +16,3 @@ PrimeFactors.prototype.isPrime = function(number) {
 PrimeFactors.prototype.isDivisible = function(number){
 	return  ((number%2==0)||(number%3==0)||(number%7==0));
 }
-
-/*
-Player.prototype.pause = function() {
-  this.isPlaying = false;
-};
-
-Player.prototype.resume = function() {
-  if (this.isPlaying) {
-    throw new Error("song is already playing");
-  }
-
-  this.isPlaying = true;
-};
-
-Player.prototype.makeFavorite = function() {
-  this.currentlyPlayingSong.persistFavoriteStatus(true);
-};*/
