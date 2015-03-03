8b9b32fc31c70ab4eee5469fe1bf0a89ed028fcc
Ruben <ruben@ruben-Aspire-6530G.(none)>
2015-03-03 10:50:35 +0100

    Added Test for number 55

--- spec/PrimeFactorsSpec.js
+++ spec/PrimeFactorsSpec.js
@@ -30,48 +30,7 @@ describe("PrimeFactors", function() {
 	it("shold return false when number is 49", function() {
 		expect(primeFactors.isPrime(49)).toEqual(false);			
 	});
-/*
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
-    });
-
-    it("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
-
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
-    });
-
-    it("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
-    });
+  it('should return false when number is 55', function() {
+    expect(primeFactors.isPrime(55)).toEqual(false);
   });
-*/
-/*
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
-*/
-/*
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrowError("song is already playing");
-    });
-  });
-*/
 });
--- src/PrimeFactors.js
+++ src/PrimeFactors.js
@@ -6,11 +6,15 @@ function PrimeFactors() {
 PrimeFactors.prototype.isPrime = function(number) {	
 	var isPrime = false;
 	
-	if((number == 2 || number == 3) || number % 2 != 0 && number % 3 != 0) {
+	if((number == 2 || number == 3) ||!this.isDivisible(number)) {
 		isPrime = true;
 	}
 
 	return isPrime;
+}
+
+PrimeFactors.prototype.isDivisible = function(number){
+	return  ((number%2==0)||(number%3==0)||(number%7==0));
 }
 
 /*
